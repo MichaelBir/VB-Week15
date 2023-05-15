1fcd237141096fc4c7c34805df0a6bcd8f2057c2
michaelbir <mikux22@gmail.com>
2023-05-15 21:47:24 +0300

    first commit

---
+++ Week_15/Program.cs
@@ -0,0 +1,46 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+
+string[] data = GetDataFromFile();
+//ReadDataFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' } StringSplitOption.RemoveEmptyEntries);
+    Movie newMovie = new Movie (tempArray)[0], tempArray[2]);
+}
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"/Users/michael/Desktop/RKE129/VisualStudio/Data/movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+class Movie
+{
+    string title; //fields
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie(string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
